c953d68daba5fc22d8c041736061625808996168
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-07 23:26:29 +0200

    add  modifications 02_filtering.py

--- 02_filtering.py
+++ 02_filtering.py
@@ -4,6 +4,9 @@ import openpyxl
 #read CSV
 df = pd.read_csv("dataset/world_population.csv") #SyntaxError: Non-UTF-8 code starting with '\xff'
 
+filtre = df["Rank"]>50
+#print(df[filtre]) #[184 rows x 17 columns]
 
-print(df)
+#or
+print(df[df["Rank"]>50]) #[184 rows x 17 columns]
 
