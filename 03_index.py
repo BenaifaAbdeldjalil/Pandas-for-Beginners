7a3dee2df5a34994b17492032bb60af29cdb7a7e
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-12 12:35:00 +0200

    add modifications 03_index.py

--- 03_index.py
+++ 03_index.py
@@ -17,3 +17,36 @@ df.reset_index(inplace=True)
 ##make CCA3 as index
 df.set_index("CCA3",inplace=True)
 print(df[:2])
+
+#loc #value ex=ALB should be in the index column
+print(df.loc["ALB"])
+
+#iloc is the same thins but by index (integer) index location==>iloc
+print(df.iloc[2])
+
+#multipl index
+#reset index
+df.reset_index(inplace=True)
+df.set_index(['Continent',"Country"],inplace=True)
+
+#order index
+df.sort_index(ascending=[True,False],inplace=True)
+print(df[:10])
+df.sort_index(ascending=[False,True],inplace=True)
+print(df[:10])
+df.sort_index(inplace=True)
+print(df[:10])
+
+print(df.loc['Africa','Algeria'])
+
+
+#but 
+print(df.iloc[1]) #Name: (Africa, Angola), dtype: objec
+#because loc and iloc don't work in same logique 
+
+
+
+
+
+
+
