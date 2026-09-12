f14dc76117d5ff9a0bbe6cfdc1d1c434bfd70363
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-12 18:39:33 +0200

    add modifications 05_merging.py

--- 05_merging.py
+++ 05_merging.py
@@ -2,5 +2,12 @@ import pandas as pd
 import openpyxl
 
 #read CSV
-df = pd.read_csv("dataset/Flavors.csv", sep=",") 
-#print(df[:2])
+df1 = pd.read_csv("dataset/LOTR.csv", sep=",") 
+df2 = pd.read_csv("dataset/LOTR 2.csv", sep=",") 
+print(df1[:2])
+print(df2[:2])
+
+
+#merge
+df3 = df1.merge(df2)
+print(df3)
