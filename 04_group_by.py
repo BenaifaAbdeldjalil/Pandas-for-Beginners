3ff086375ec17f3f0c12aba86072a509cc07b3e5
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-12 14:18:02 +0200

    add modifications 04_group_by.py

--- 04_group_by.py
+++ 04_group_by.py
@@ -14,11 +14,32 @@ df_grouby.mean(numeric_only=True)
 df_grouby.count()
 df_grouby = df.groupby('Base Flavor').max()
 df_grouby = df.groupby('Base Flavor').min()
+df.groupby('Base Flavor').agg({'Flavor Rating':['mean','max','min','count']})
 
+
+df_grouby = df.groupby(['Base Flavor','Liked']).mean(numeric_only=True)
 
 #OR
 df1=df.groupby('Base Flavor').mean(numeric_only=True)
 print(df_grouby.mean(numeric_only=True))
 print(df.groupby('Base Flavor').min())
 print(df.groupby('Base Flavor').max())
+print(df.groupby('Base Flavor').agg({'Flavor Rating':['mean','max','min','count']}))
+print(df.groupby(['Base Flavor','Liked']).mean(numeric_only=True))
+print(df.groupby(['Base Flavor','Liked']).max())
+
+#describe
+print(df.groupby(['Base Flavor','Liked']).describe().columns)
+
+#
+
+
+
+
+
+
+
+
+
+
 
