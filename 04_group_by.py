e1211162abbd96ca7358bd15c3b6ef3aff6ac2de
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-12 13:56:15 +0200

    add modifications 04_group_by.py

--- 04_group_by.py
+++ 04_group_by.py
@@ -2,10 +2,23 @@ import pandas as pd
 import openpyxl
 
 #read CSV
-#df = pd.read_csv("dataset/world_population.csv", sep=",") 
+df = pd.read_csv("dataset/Flavors.csv", sep=",") 
 #print(df[:2])
 
-#index 
-#make country as index
-df = pd.read_csv("dataset/world_population.csv",index_col="Country", sep=",") 
-print(df[:2])
+#groupby
+df_grouby = df.groupby('Base Flavor')
+
+#groupby methode
+df_grouby.sum()
+df_grouby.mean(numeric_only=True)
+df_grouby.count()
+df_grouby = df.groupby('Base Flavor').max()
+df_grouby = df.groupby('Base Flavor').min()
+
+
+#OR
+df1=df.groupby('Base Flavor').mean(numeric_only=True)
+print(df_grouby.mean(numeric_only=True))
+print(df.groupby('Base Flavor').min())
+print(df.groupby('Base Flavor').max())
+
