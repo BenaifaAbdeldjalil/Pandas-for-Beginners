bbf367646bfb3760a9950f1b0dbec89dd5a6a2b7
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-13 14:07:50 +0200

    add modifications git.sh

--- git.sh
+++ git.sh
@@ -22,12 +22,12 @@ git add .
 echo "Commiting  file : $PARAM ...."
 echo 
 # Commit avec message personnalisé
-git commit -m "add modifications $PARAM"
+git commit -m "add modifications $PARAM $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') "
 echo "Commit OK!"
 echo 
 echo 
 
-echo "Pushing file : $PARAM ..."
+echo "Pushing file : $PARAM ... $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') "
 echo 
 # Push
 git push
